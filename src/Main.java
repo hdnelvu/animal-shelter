bbf7adbdb2b4d103cd1c2b6543d7a21c1edc7984
hdnelvu <notnelvu@gmail.com>
2023-12-26 18:53:36 +0100

    dsa

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,8 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-
+        Shelter shelter = Shelter.getInstance();
+        shelter.init("Nice shelter :3");
+        System.out.println(shelter.getShelterName());
     }
 }
